1fdf58a134793f923b9d29e6650ad5a873e1ba6a
mohammadrezabahrani <bahrani.mohammadreza@yahoo.com>
2023-06-10 19:25:15 +0330

    eslah

--- MultivariableRegression.cpp
+++ MultivariableRegression.cpp
@@ -1,36 +1,72 @@
 #include <iostream>
 #include <vector>
 
-// Function to calculate multivariable regression coefficients
 std::vector<double> multivariableRegression(const std::vector<std::vector<double>>& X, const std::vector<double>& Y) {
     int numVariables = X[0].size();  // Number of independent variables
     int numSamples = X.size();  // Number of data samples
 
-    // Calculate sums and cross-products
-    std::vector<double> sumX(numVariables, 0.0);
+    // Calculate means
+    std::vector<double> means(numVariables, 0.0);
+    double meanY = 0.0;
+
+    for (int j = 0; j < numVariables; j++) {
+        for (int i = 0; i < numSamples; i++) {
+            means[j] += X[i][j];
+        }
+        means[j] /= numSamples;
+    }
+
+    for (int i = 0; i < numSamples; i++) {
+        meanY += Y[i];
+    }
+    meanY /= numSamples;
+
+    // Calculate cross-products
     std::vector<double> sumXY(numVariables, 0.0);
-    double sumY = 0.0;
-    double sumX2 = 0.0;
 
     for (int i = 0; i < numSamples; i++) {
         for (int j = 0; j < numVariables; j++) {
-            sumX[j] += X[i][j];
             sumXY[j] += X[i][j] * Y[i];
         }
-        sumY += Y[i];
-        sumX2 += X[i][0] * X[i][0];  // Assuming the first variable is X1
+    }
+
+    // Calculate sum of squares
+    std::vector<double> sumX2(numVariables, 0.0);
+
+    for (int i = 0; i < numSamples; i++) {
+        for (int j = 0; j < numVariables; j++) {
+            sumX2[j] += X[i][j] * X[i][j];
+        }
     }
 
     // Calculate regression coefficients
     std::vector<double> coefficients(numVariables, 0.0);
-    double denominator = numSamples * sumX2 - sumX[0] * sumX[0];  // Assuming the first variable is X1
+    std::vector<double> sumX(numVariables, 0.0);
+
+    for (int i = 0; i < numSamples; i++) {
+        for (int j = 0; j < numVariables; j++) {
+            sumX[j] += X[i][j];
+        }
+    }
 
     for (int i = 0; i < numVariables; i++) {
-        double numerator = numSamples * sumXY[i] - sumX[i] * sumY;
+        double denominator = numSamples * sumX2[i] - sumX[i] * sumX[i];
+        double numerator = numSamples * sumXY[i] - sumX[i] * meanY;
         coefficients[i] = numerator / denominator;
     }
 
-    return coefficients;
+    // Calculate intercept term
+    double intercept = meanY;
+
+    for (int i = 0; i < numVariables; i++) {
+        intercept -= coefficients[i] * means[i];
+    }
+
+    std::vector<double> result;
+    result.push_back(intercept);
+    result.insert(result.end(), coefficients.begin(), coefficients.end());
+
+    return result;
 }
 
 int main() {
@@ -44,13 +80,17 @@ int main() {
 
     std::vector<double> Y = {15, 17, 15, 18, 20};
 
-    // Calculate regression coefficients
-    std::vector<double> coefficients = multivariableRegression(X, Y);
+    // Calculate regression coefficients and intercept
+    std::vector<double> result = multivariableRegression(X, Y);
 
-    // Print the regression coefficients
+    double intercept = result[0];
+    std::vector<double> coefficients(result.begin() + 1, result.end());
+
+    // Print the intercept and regression coefficients
+    std::cout << "Intercept (a): " << intercept << std::endl;
     std::cout << "Regression Coefficients:" << std::endl;
     for (int i = 0; i < coefficients.size(); i++) {
-        std::cout << "b" << i+1 << " = " << coefficients[i] << std::endl;
+        std::cout << "b" << i + 1 << " = " << coefficients[i] << std::endl;
     }
 
     return 0;
